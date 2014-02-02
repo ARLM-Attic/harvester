6d013296dc8cddee436bb12eeb738064bad31356
CBaxter <Chris.Baxter@shaw.ca>
2014-02-01 17:36:09 -0700

    Sync with GitHub codebase (v2.0.2 release).

--- package.cmd
+++ package.cmd
@@ -1,17 +1,8 @@
 @ECHO OFF
 
 set MSBuildExe="%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-set GitExe="%SYSTEMDRIVE%\Program Files (x86)\SmartGitHg 4\git\bin"
 
 IF NOT EXIST %MSBuildExe% GOTO MSBuildNotFound
-IF NOT EXIST %GitExe% GOTO GitNotFound
-
-:: Load HEAD commit in to parameter
-:: --------------------------------------------------
-%GitExe% rev-parse HEAD > .gitcommit
-set /p Commit= < .gitcommit
-del .gitcommit
-echo Commit: %Commit%
 
 :: Prompt for product version
 :: --------------------------------------------------
